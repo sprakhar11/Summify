f84e742c187ee740c4ddede849206bdc5512eb9c
Prakhar Sharma <76238592+sprakhar11@users.noreply.github.com>
2021-08-16 01:53:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var btn=document.getElementById("input3");
+btn.onclick = function () {
+     var x = document.getElementById("output1");
+//   if (x.style.display == "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   } 
+ x.value = `Title: Meditation Cures Stress and Insomnia
+ Stress and sleep deprivation is a major problem for sound health and sound mind. So, it interferes with the performance of our memory and immune system. It also reinforces our negative outlook and bad mood. So, our best ideas and memories can come to us only if we have good sleep and stress free life. In. fact, stress management can maximize opr brain power. Hence, the hour of need is to practise meditation in order to solve the issue of stress and sleep deprivation. Above all, meditation is a bliss to gain health benefits and religious and spiritual life.
+ SUMMARY IN POINTERS:
+ •Problems of sleep deprivation
+                                         
+ I. Lack of a good night’s sleep
+  (i) affects our health and mind
+  (ii) is a problem of aging stress
+  (iii) causes mental problem
+  (iv) intrfrs with performance of our memory
+                                         
+ II. Good memory and mood
+  (i) need good sleep
+  (ii) can improve your immune system
+  (iii) help to have your +ve outlook on life
+  (iv) can save you from insomnia
+                                         
+ III. Regular meditation
+  (a) has health benefits
+  (b) brings about good sleep
+  (c) helps reducing stress
+  (d) empowers your religious and sports life`
+}
